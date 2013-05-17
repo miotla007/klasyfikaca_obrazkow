37863cdad1fca47ab2bd9f2bf23608615b35d802
Mateusz <mmotlawski@sigma.ug.edu.pl>
2013-05-18 00:44:06 +0200

    Added beta application to repository

---
+++ app.js
@@ -0,0 +1,39 @@
+/*jshint node: true */
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var app = express();
+
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+var server = http.createServer(app).listen(app.get('port'), function () {
+    console.log("Serwer nasłuchuje na porcie " + app.get('port'));
+});
+
+var io = require('socket.io');
+var socket = io.listen(server);
+
+var imgDb = require('./lib/imgDb')();
+
+socket.on('connection', function (client) {
+    'use strict';
+
+    var imgs = imgDb.getAllImgs();
+    for(var i = 0; i < imgs.length; i++){
+        client.emit('setNewImg', imgs[i]);
+    }
+
+    client.on('addNewImg', function(data) {
+        var id = imgDb.addNewImg(data);
+        data.id = id;
+        // console.log(data);
+        client.emit('setNewImg', imgDb.getImgById(id));
+        client.broadcast.emit('setNewImg', imgDb.getImgById(id));
+    });
+    
+});
---
+++ public/js/skrypt.js
@@ -0,0 +1,69 @@
+/*jslint browser: true, sloppy: true */
+/*global $: false */
+
+$(document).ready(function () {
+
+    var socket = io.connect('http://localhost:3000');
+
+    var addNewSelectedItem = function (data){
+        return '<tr id="item'+data.id+'" hidden="true"><td><img class="img-polaroid" src="'+ data.img +'"></img></td>'+
+        '<td class="rank">'+data.rank+'</td>'+
+                    '<td><button class="btn btn-warning" id="itemBtn'+data.id+'">VOTE</button></td></tr>';
+    }
+
+    var voteListBtnClick = function(that) {
+        var id = $(that).attr('id');
+        // console.log(id);
+        var img = $('#img'+id).attr('src');
+        // console.log(img);
+
+        var data = {};
+        data.src = img;
+        //data.id = id;
+
+        // console.log(addNewSelectedItem(data));
+        socket.emit('addNewImg', data);
+    }
+
+    var voteItemBtnClick = function(that){
+        var orgId = $(that).attr('id');
+        var id = orgId.substring(7, orgId.length);
+        // console.log(id);
+        var img = $('#item'+id+' img').attr('src');
+        // console.log(img);
+
+        socket.emit('addNewImg', {'src': img});
+    }
+    $('#buttonTag').click(function () {
+        var value = document.getElementById("appendedInputButton").value;
+        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
+            tags: value,
+            tagmode: "any",
+            format: "json"
+        }, function (data) {
+            $.each(data.items, function (i, item) {
+                console.log(item.media.m);
+                $('#responseOutput').append('<tr><td><img id="img'+i+'" class="img-polaroid" src="'+ item.media.m +'"></img><td></td>'+
+                    '<td><button class="btn btn-success" id="'+i+'">SELECT AND VOTE</button></td></tr>');
+                if (i === 5) {
+                    return false;
+                }
+            });
+            $('#responseOutput button').click(function(){ voteListBtnClick(this);  });
+        });
+    });
+
+    socket.on('setNewImg', function(data) {
+        console.log(data);
+        var items = $('#selectedItems tr').filter('#item'+data.id);
+        if(items.length === 0){
+            $('#selectedItems').append(addNewSelectedItem(data));
+            $('#item'+data.id).slideDown('slow', function(){
+                $('#item'+data.id+' button').click(function() { voteItemBtnClick(this); });
+            });
+            
+        }else{
+            $('#item'+data.id+' .rank').text(data.rank);
+        }
+    });
+});
